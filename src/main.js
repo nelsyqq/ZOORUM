b47162fcafd11cc8892677fc4fa9166eeb3ae886
nelsyqq <nelsynev@gmail.com>
2026-07-01 01:27:29 +0300

    Revert to @fontsource self-hosted fonts (no Google Fonts CDN)

--- src/main.js
+++ src/main.js
@@ -2,6 +2,13 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
+import '@fontsource/nunito/400.css'
+import '@fontsource/nunito/600.css'
+import '@fontsource/nunito/700.css'
+import '@fontsource/nunito/800.css'
+import '@fontsource/baloo-2/600.css'
+import '@fontsource/baloo-2/700.css'
+import '@fontsource/baloo-2/800.css'
 import './assets/main.css'
 
 const app = createApp(App)
